85ca611abb515eeb59ebe2d8ca840e9a98bca291
Md-sakib-al-hasan <mdsakibalhasanprogrammer1@gmail.com>
2023-04-08 04:25:39 +0600

    s

--- First Assimant/Fivechallange.c
+++ First Assimant/Fivechallange.c
@@ -5,7 +5,7 @@ int main ()
     if(1000<n){
         printf("I will buy Punjabi\n");
         
-        if(n-1000 > 500){
+        if(n-1000 >= 500){
         printf("I will buy new shoes\n");
         printf("Alisa will buy new shoes\n");
          }
