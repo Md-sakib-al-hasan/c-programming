195a0aca513256b980bfee26fc5ea0ff81904ec0
Md-sakib-al-hasan <mdsakibalhasanprogrammer1@gmail.com>
2023-04-07 21:00:24 +0600

    sk

--- First Assimant/Fivechallange.c
+++ First Assimant/Fivechallange.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 int main ()
-{  unsigned int a;
+{  long long int n,k;
+    scanf("%lld",&n);
+    k=n-1000;
+    if(1000<n){
+        printf("you will buy panjabi\n");
+    }if(k>500){
+        printf("you wiil bye shous\n");
+        printf("Alis will by show\n");
+    }else{
+        printf("he cnat by some ting");
+    }
   
     
     return 0;
