f1916cba9231208c41bb8c578c6c31353732f9a6
Md-sakib-al-hasan <mdsakibalhasanprogrammer1@gmail.com>
2023-04-09 19:27:57 +0600

    error

---
+++ Module  five/five_four.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+int main ()
+{
+    int a;
+    scanf("%d",&a);
+    int dev= a/1000;
+    if(dev%2==0){
+        printf("EVEN");
+    }else{
+        printf("ODD");
+    }
+ }
